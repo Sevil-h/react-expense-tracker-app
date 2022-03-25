cb596cc464f855c98d4208deaab5cb0e463dd27c
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-03-25 21:57:48 +0300

    added amounts

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Transaction({ item }) {
+  const sign = item.amount < 0 ? "-" : "+";
+
   return (
-    <li className="minus">
-      {item.text} <span>${item.amount}</span>
+    <li className={item.amount < 0 ? "minus" : "plus"}>
+      {item.text}{" "}
+      <span>
+        {sign}${Math.abs(item.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
